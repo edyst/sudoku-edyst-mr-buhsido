39f8e201c15bc0e6c2f0f1a431b54cf5d09bd3b4
bushido <nagpranav007@gmail.com>
2021-08-30 22:20:00 +0530

    update

--- algo.js
+++ algo.js
@@ -1,22 +1,22 @@
-var duplicacy = Array(3); // 0 row; 1 col; 2 box
+// var duplicacy = Array(3); // 0 row; 1 col; 2 box
 
-for (let i = 0; i < 3; i++) {
-	duplicacy[i] = new Array(9);
-}
+// for (let i = 0; i < 3; i++) {
+// 	duplicacy[i] = new Array(9);
+// }
 
-for (let i = 0; i < 3; i++) {
-	for (let j = 0; j < 9; j++) {
-		duplicacy[i][j] = new Array(10);
-	}
-}
+// for (let i = 0; i < 3; i++) {
+// 	for (let j = 0; j < 9; j++) {
+// 		duplicacy[i][j] = new Array(10);
+// 	}
+// }
 
-for (let i = 0; i < 3; i++) {
-	for (let j = 0; j < 9; j++) {
-		for (let k = 0; k < 10; k++) {
-			duplicacy[i][j][k] = 0;
-		}
-	}
-}
+// for (let i = 0; i < 3; i++) {
+// 	for (let j = 0; j < 9; j++) {
+// 		for (let k = 0; k < 10; k++) {
+// 			duplicacy[i][j][k] = 0;
+// 		}
+// 	}
+// }
 
 function getBox(row, col) {
 	let R = parseInt(parseInt(row) / 3);
@@ -25,40 +25,41 @@ function getBox(row, col) {
 	return parseInt(R * 3 + C);
 }
 
-function validate(row, col, val) {
-	let box = getBox(row, col);
-	let id = "a" + row + col;
+// function validate(row, col, val) {
+// 	let box = getBox(row, col);
+// 	let id = "a" + row + col;
 
-	let el = document.getElementById(id);
+// 	let el = document.getElementById(id);
 
-	if (duplicacy[0][row][val] === 0) {
-		//duplicacy[0][row][val]++;
-		white(el);
-	} else {
-		red(el);
-	}
-	duplicacy[0][row][val]++;
+// 	if (duplicacy[0][row][val] === 0) {
+// 		//duplicacy[0][row][val]++;
+// 		white(el);
+// 	} else {
+// 		red(el);
+// 	}
+// 	duplicacy[0][row][val]++;
 
-	if (duplicacy[1][col][val] === 0) {
-		//duplicacy[1][col][val]++;
-		white(el);
-	} else {
-		red(el);
-	}
-	duplicacy[1][col][val]++;
+// 	if (duplicacy[1][col][val] === 0) {
+// 		//duplicacy[1][col][val]++;
+// 		white(el);
+// 	} else {
+// 		red(el);
+// 	}
+// 	duplicacy[1][col][val]++;
 
-	if (duplicacy[2][box][val] === 0) {
-		//duplicacy[2][box][val]++;
-		white(el);
-	} else {
-		//duplicacy[2][box][val]++;
-		red(el);
-	}
-	duplicacy[2][box][val]++;
-}
+// 	if (duplicacy[2][box][val] === 0) {
+// 		//duplicacy[2][box][val]++;
+// 		white(el);
+// 	} else {
+// 		//duplicacy[2][box][val]++;
+// 		red(el);
+// 	}
+// 	duplicacy[2][box][val]++;
+// }
 
 function red(el) {
 	el.className = "cellsErr";
+	el.value = "";
 }
 
 function white(el) {
@@ -75,31 +76,74 @@ function processNo(row, col) {
 	}
 }
 
-function validateAll() {
-	console.log("validateAll starts");
+// function validateAll() {
+// 	console.log("validateAll starts");
 
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 9; j++) {
-			for (let k = 0; k < 10; k++) {
-				duplicacy[i][j][k] = 0;
-			}
-		}
+// 	for (let i = 0; i < 3; i++) {
+// 		for (let j = 0; j < 9; j++) {
+// 			for (let k = 0; k < 10; k++) {
+// 				duplicacy[i][j][k] = 0;
+// 			}
+// 		}
+// 	}
+
+// 	for (let i = 0; i < 9; i++) {
+// 		for (let j = 0; j < 9; j++) {
+// 			processNo(i, j);
+// 		}
+// 	}
+
+// 	console.log("validateAll ends");
+// }
+
+function isNumber(row, col) {
+	let el = document.getElementById("a" + row + col);
+	white(el);
+	let no = el.value;
+	if (no >= 0 && no <= 9) {
+		sudoku(row, col, no, el);
+	} else {
+		el.value = "";
 	}
-
-	for (let i = 0; i < 9; i++) {
-		for (let j = 0; j < 9; j++) {
-			processNo(i, j);
-		}
-	}
-
-	console.log("validateAll ends");
 }
 
-function isNumber(id) {
-	let el = document.getElementById(id);
-	let no = el.value;
-	if (!(no >= 0 && no <= 9)) {
-		el.value = "";
+function sudoku(row, col, val, me) {
+	white(me);
+	// row
+	for (let i = 0; i < 9; i++) {
+		let r = parseInt((parseInt(row) + i) % 9);
+		let el = document.getElementById("a" + r + col);
+		if (el.value === val) {
+			red(me); // pro max
+		}
+	}
+
+	// col
+	for (let i = 0; i < 9; i++) {
+		let c = parseInt((parseInt(col) + i) % 9);
+		let el = document.getElementById("a" + row + c);
+		if (el.value === val) {
+			red(me); // pro max
+		}
+	}
+
+	// box
+	let starterRow = parseInt(row) / 3;
+	let starterCol = parseInt(col) / 3;
+
+	starterRow = starterRow * 3;
+	starterCol = starterCol * 3;
+
+	for (let i = 0; i < 3; i++) {
+		let r = parseInt(starterRow) + parseInt(i);
+		for (let j = 0; j < 3; j++) {
+			let c = parseInt(starterCol) + parseInt(j);
+			let el = document.getElementById("a" + r + c);
+
+			if (el.value === val) {
+				red(me); // pro max
+			}
+		}
 	}
 }
 
